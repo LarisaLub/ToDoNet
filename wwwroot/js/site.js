b4993a96f6216f7c0d48a87f0715821f4bae939f
Larisa <llubonko@gmail.com>
2020-04-06 23:29:02 +0300

    add JavaScript code

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.addEventListener("load", function (event) {
+    let el = document.querySelector(".save");
+    let elLabel = document.querySelector('.label');
+    el.addEventListener('click', () => {
+        let elInput = document.querySelector("input.content");
+        if (elInput.value === '') {
+            alert('Empty field');
+            return;
+        }
+        elLabel.innerText = elInput.value;
+        elInput.value = '';
+    })
+
+    document.querySelector('.todo-button-delete').addEventListener('click', () => {
+        elLabel.innerText = ''
+    });
+});
