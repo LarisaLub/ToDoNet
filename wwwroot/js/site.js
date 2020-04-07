f8ef9ed6342fa684979df46cfe24893ad0fd37ab
Larisa <llubonko@gmail.com>
2020-04-08 00:26:07 +0300

    fixed code JS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,22 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-window.addEventListener("load", function (event) {
-    let el = document.querySelector(".save");
-    let elLabel = document.querySelector('.label');
-    el.addEventListener('click', () => {
-        let elInput = document.querySelector("input.content");
+﻿window.addEventListener("load", function (event) {
+    var el = document.querySelector(".save");
+    var elLabel = document.querySelector('.label');
+    el.addEventListener('click', function () {
+        var elInput = document.querySelector("input.content");
         if (elInput.value === '') {
             alert('Empty field');
             return;
         }
         elLabel.innerText = elInput.value;
         elInput.value = '';
-    })
+    });
 
-    document.querySelector('.todo-button-delete').addEventListener('click', () => {
-        elLabel.innerText = ''
+    document.querySelector('.todo-button-delete').addEventListener('click', function () {
+        elLabel.innerText = '';
     });
 });
